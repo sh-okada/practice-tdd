5eaa74bf807e46238df3eceb6dd130537eb07533
sh-okada <sh-okada@ymail.ne.jp>
2025-11-16 20:48:14 +0900

    #1 render -> renderApp

--- nextjs/app/__tests__/components/ui/PasswordField.spec.tsx
+++ nextjs/app/__tests__/components/ui/PasswordField.spec.tsx
@@ -1,39 +1,40 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
 import { PasswordField } from "@/components/ui/PasswordField";
+import { renderApp } from "@/libs/rtl";
 
 describe("目のアイコンでパスワードの表示・非表示を切り替える", () => {
   const renderComponent = () =>
-    render(<PasswordField value="password" label="パスワード" />);
+    renderApp(<PasswordField value="password" label="パスワード" />);
 
   describe("パスワードを非表示にしている場合", () => {
     test("目のアイコンをクリックするとパスワードが表示されること", async () => {
-      renderComponent();
+      const { findByLabelText } = renderComponent();
 
-      const showPasswordIconButton = await screen.findByLabelText(
+      const showPasswordIconButton = await findByLabelText(
         "パスワードを表示する",
       );
       fireEvent.click(showPasswordIconButton);
 
-      const passwordField = await screen.findByLabelText("パスワード");
+      const passwordField = await findByLabelText("パスワード");
       expect(passwordField).toHaveAttribute("type", "text");
     });
   });
 
   describe("パスワードを表示している場合", () => {
     test("目のアイコンをクリックするとパスワードが非表示になること", async () => {
-      renderComponent();
+      const { findByLabelText } = renderComponent();
 
-      const showPasswordIconButton = await screen.findByLabelText(
+      const showPasswordIconButton = await findByLabelText(
         "パスワードを表示する",
       );
       fireEvent.click(showPasswordIconButton);
 
-      const hidePasswordIconButton = await screen.findByLabelText(
+      const hidePasswordIconButton = await findByLabelText(
         "パスワードを非表示にする",
       );
       fireEvent.click(hidePasswordIconButton);
 
-      const passwordField = await screen.findByLabelText("パスワード");
+      const passwordField = await findByLabelText("パスワード");
       expect(passwordField).toHaveAttribute("type", "password");
     });
   });
