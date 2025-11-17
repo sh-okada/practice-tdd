989aa3aeb82ea0f7526467218646b1b2c98c9cb2
Shuta-Okada <sh-okada@enecom.co.jp>
2025-11-17 17:12:33 +0900

    #1 テストケースのリファクタリング

--- nextjs/app/__tests__/components/providers/AuthProvider.spec.tsx
+++ nextjs/app/__tests__/components/providers/AuthProvider.spec.tsx
@@ -1,78 +1,85 @@
-import { Typography } from "@mui/material";
+import { waitFor } from "@testing-library/dom";
 import { HttpResponse, http } from "msw";
 import { AuthProvider } from "@/components/providers";
 import { server } from "@/libs/msw";
 import { renderApp } from "@/libs/rtl";
 
-const renderComponent = () =>
-  renderApp(
-    <AuthProvider>
-      {(isAuthed) => (
-        <Typography>
-          {isAuthed ? "認証しています" : "認証していません"}
-        </Typography>
-      )}
-    </AuthProvider>,
-  );
+describe("ログイン状態で異なるコンポーネントを表示する", () => {
+  let expected: boolean;
+  const renderComponent = () =>
+    renderApp(
+      <AuthProvider>
+        {(isAuthed) => {
+          expected = isAuthed;
 
-describe("ユーザー情報取得APIでログイン状態をチェックし、ログイン状態によってコンポーネントを切り替える", () => {
-  describe("APIのステータスコードが200の場合", () => {
-    test("ログイン状態であること", async () => {
-      server.use(
-        http.get("http://localhost:8000/api/users/me", () => {
-          return HttpResponse.json(
-            {
-              id: "8415e241-9a24-4502-847a-abe348e84535",
-              email: "hoge@example.com",
-              name: "hoge",
-            },
-            { status: 200 },
-          );
-        }),
-      );
+          return null;
+        }}
+      </AuthProvider>,
+    );
 
-      const { findByText } = renderComponent();
+  describe("ユーザー情報取得APIの結果によってログイン状態を決める", () => {
+    describe("APIのステータスコードが200の場合", () => {
+      test("isAuthedがTrueであること", async () => {
+        server.use(
+          http.get("http://localhost:8000/api/users/me", () => {
+            return HttpResponse.json(
+              {
+                id: "8415e241-9a24-4502-847a-abe348e84535",
+                email: "hoge@example.com",
+                name: "hoge",
+              },
+              { status: 200 },
+            );
+          }),
+        );
 
-      expect(await findByText("認証しています")).toBeInTheDocument();
+        renderComponent();
+
+        await waitFor(() => {
+          expect(expected).toBe(true);
+        });
+      });
     });
-  });
 
-  describe("APIのステータスコードが401の場合", () => {
-    test("ログイン状態でないこと", async () => {
-      server.use(
-        http.get("http://localhost:8000/api/users/me", () => {
-          return HttpResponse.json(
-            {
-              detail: "認証していません。",
-            },
-            { status: 401 },
-          );
-        }),
-      );
+    describe("APIのステータスコードが401の場合", () => {
+      test("isAuthedがFalseであること", async () => {
+        server.use(
+          http.get("http://localhost:8000/api/users/me", () => {
+            return HttpResponse.json(
+              {
+                detail: "認証していません。",
+              },
+              { status: 401 },
+            );
+          }),
+        );
 
-      const { findByText } = renderComponent();
+        renderComponent();
 
-      expect(await findByText("認証していません")).toBeInTheDocument();
+        await waitFor(() => {
+          expect(expected).toBe(false);
+        });
+      });
     });
-  });
 
-  describe("それ以外のステータスコードの場合", () => {
-    test("エラーが表示されること", async () => {
-      server.use(
-        http.get("http://localhost:8000/api/users/me", () => {
-          return HttpResponse.json(
-            {
-              detail: "サーバーでエラーが発生しました。",
-            },
-            { status: 500 },
-          );
-        }),
-      );
+    describe("それ以外のステータスコードの場合", () => {
+      test("エラーが表示されること", async () => {
+        server.use(
+          http.get("http://localhost:8000/api/users/me", () => {
+            return HttpResponse.json(
+              {
+                detail: "サーバーでエラーが発生しました。",
+              },
+              { status: 500 },
+            );
+          }),
+        );
 
-      const { findByText } = renderComponent();
+        const { findByText } = renderComponent();
 
-      const message = await findByText("予期しないエラーが発生しました。");
-      expect(message).toBeInTheDocument();
+        const message = await findByText("予期しないエラーが発生しました。");
+        expect(message).toBeInTheDocument();
+      });
     });
   });
 });
