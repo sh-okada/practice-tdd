0d056e8267d0d3b87089586343b06d65cec0fd29
sh-okada <sh-okada@ymail.ne.jp>
2025-11-29 23:15:55 +0900

    storybookのバージョン更新

--- nextjs/app/.storybook/main.ts
+++ nextjs/app/.storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@chromatic-com/storybook", "@storybook/addon-docs"],
   framework: {
-    name: "@storybook/nextjs",
+    name: "@storybook/nextjs-vite",
     options: {},
   },
   staticDirs: ["../public"],
