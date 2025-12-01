3cfc2e8b4bdf7eabe0e3ed6d2b9b3b3d7195b6c6
Shuta-Okada <sh-okada@enecom.co.jp>
2025-12-01 09:45:01 +0900

    不要なテスト削除

--- nextjs/app/__tests__/components/providers/AuthProvider.spec.tsx
+++ nextjs/app/__tests__/components/providers/AuthProvider.spec.tsx
@@ -1,52 +1,8 @@
-import { Typography } from "@mui/material";
 import { waitFor } from "@testing-library/dom";
-import { delay, HttpResponse, http } from "msw";
+import { HttpResponse, http } from "msw";
 import { AuthProvider } from "@/components/providers";
 import { server } from "@/libs/msw";
 import { renderApp } from "@/libs/rtl";
-
-describe("API実行中はローディングを表示する", () => {
-  const renderComponent = () =>
-    renderApp(
-      <AuthProvider>
-        {() => <Typography>読み込み完了</Typography>}
-      </AuthProvider>,
-    );
-
-  beforeEach(() => {
-    server.use(
-      http.get("http://localhost:8000/api/users/me", async () => {
-        await delay(500);
-
-        return HttpResponse.json(
-          {
-            id: "8415e241-9a24-4502-847a-abe348e84535",
-            email: "hoge@example.com",
-            name: "hoge",
-          },
-          { status: 200 },
-        );
-      }),
-    );
-  });
-
-  describe("API実行中の場合", () => {
-    test("ローディングが表示されること", async () => {
-      const { findByLabelText } = renderComponent();
-
-      expect(await findByLabelText("読み込み中"));
-    });
-  });
-
-  describe("API実行完了の場合", () => {
-    test("ローディングが表示されないこと", async () => {
-      const { queryByLabelText, findByText } = renderComponent();
-
-      await findByText("読み込み完了");
-      expect(queryByLabelText("読み込み中")).toBeNull();
-    });
-  });
-});
 
 describe("ユーザー情報取得APIの結果でログイン状態を判定する", () => {
   let expected: boolean;
